8c6410b415c10a0e101e32767fc87bc3e6c40b3d
Gabriel Dantas <gabriel.dantas@catskillet.com>
2021-04-21 09:34:22 -0300

    documentacao swagger para rotas /categories

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import routes from './routes'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json'
 
 const app = express()
 const PORT = 3333
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(routes)
 
